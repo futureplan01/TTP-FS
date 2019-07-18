9bb9ce1ebe756b0eb288575063d79be8dd959371
Jesaiah <jesaiahprayor@gmail.com>
2019-07-17 20:41:04 -0400

    alot

--- client/src/Components/Portfolio.js
+++ client/src/Components/Portfolio.js
@@ -5,10 +5,19 @@ class Portfolio extends Component{
     
     constructor (props){
         super(props);
-        this.state = {stockArray: [], symbol: '', price:0,size: 1};
+        this.state = {stockArray: [], symbol: '', price:0,size: 1,error:false};
         this.getStocks = this.getStocks.bind(this);
         this.stockClick = this.stockClick.bind(this);
+        this.selectStock = this.selectStock.bind(this);
         this.buyStock = this.buyStock.bind(this);
+    }
+    buyStock(){
+        if(this.state.price > this.props.getAccount()|| !Number.isInteger(this.state.value) ){
+            this.setState({error:true});
+        }else{
+            // Buy the Stock
+
+        }
     }
     getStocks(){
         jsonp('https://ws-api.iextrading.com/1.0/tops/last',null,(err,data)=>{
@@ -19,7 +28,7 @@ class Portfolio extends Component{
             }
         })
     }
-    buyStock(e){
+    selectStock(e){
         if(!e.target.value == ''){
             let newPrice = this.state.price;
             newPrice *= e.target.value;
@@ -38,7 +47,16 @@ class Portfolio extends Component{
     componentDidMount(){
 
     }
+    ErrorMessage(){
+
+    }
     render(){
+        let ErrorMessage;
+        if(this.state.error){
+            ErrorMessage = <div> 
+                Please Make Sure You Have An is Integer And Enough Cash!
+            </div>
+        }
         if(this.state.stockArray.length ==0)
             this.getStocks();
         return (
@@ -64,12 +82,13 @@ class Portfolio extends Component{
                     <div>
                         <div id='Purchase'>Cash - {this.props.getAccount()}</div>
                         <br/>
-                        <input value ={this.state.price} className = 'input'/>
+                        <input readOnly value ={this.state.price} className = 'input'/>
                         <br/>
-                        <input onChange={this.buyStock} value={this.state.size} className = 'input'/>
+                        <input onChange={this.selectStock} value={this.state.size} className = 'input'/>
                         <br/>
                         <button className = 'button'> BUY</button>
                     </div>
+                    {ErrorMessage}
                 </div>
             </div>
             </div>
